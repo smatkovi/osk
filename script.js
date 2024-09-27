76cf49f225dd4f60a8eccde9293ea55c59df1983
sm <sebastianmatkovich@gmail.com>
2024-09-27 12:52:36 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener('keydown', function(event) {
+    const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
+    if (key) {
+        key.classList.add('pressed');
+    }
+});
+
+document.addEventListener('keyup', function(event) {
+    const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
+    if (key) {
+        key.classList.remove('pressed');
+    }
+});
+
